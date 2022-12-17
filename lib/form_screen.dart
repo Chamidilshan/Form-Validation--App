247dae8529e9ea9093ac1aaf9c141d7743b6442d
Your Name <you@example.com>
2022-12-17 20:49:16 +0530

    First edit

---
+++ lib/form_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Page'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(),
+      ),
+    );
+  }
+}
